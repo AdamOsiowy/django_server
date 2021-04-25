7deb3fd5dc6953dc3189080b2c9fedfc2bd10ccc
AdamOsiowy123 <adaosi@st.amu.edu.pl>
2021-04-25 22:18:21 +0200

    add repository view

--- django_server/repos_viewer/urls.py
+++ django_server/repos_viewer/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home_view, name="home"),
     path('<str:name>', views.user_view, name='user'),
     path('<str:name>/', views.user_view, name='user/'),
+    path('<str:name>/<str:repo_name>', views.repo_view, name='repo'),
+    path('<str:name>/<str:repo_name>/', views.repo_view, name='repo/'),
 ]
--- django_server/repos_viewer/views.py
+++ django_server/repos_viewer/views.py
@@ -29,3 +29,24 @@ def user_view(req, name):
 
     return render(req, "repos_viewer/user.html",
                   {"user_name": name, "json_list": repos_list_json, "sum_of_stars": sum_of_stars})
+
+
+def repo_view(req, name, repo_name):
+    print(f"got request: {req.path}")
+
+    request = requests.get(GITHUB_API_URL + '/repos/' + name + '/' + repo_name)
+
+    if request.status_code == 404:
+        return HttpResponse(content=render(request, "repos_viewer/error404.html"),
+                            content_type='text/html; charset=utf-8',
+                            status=404)
+    if request.status_code == 500:
+        return HttpResponse(content=render(request, "repos_viewer/error500.html"),
+                            content_type='text/html; charset=utf-8',
+                            status=500)
+    if request.status_code == 403:
+        return HttpResponse("api requests limit reached", status=403)
+
+    stars = getRepoStars(name, repo_name, request)
+    return render(req, "repos_viewer/repo.html",
+                  {"user_name": name, "repo_name": repo_name, "stars": stars})
